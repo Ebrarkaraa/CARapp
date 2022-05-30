71c0debfdefb4c572bc720ede42e1e6b800e78e9
Şüheda <58470092+Ebrarkaraa@users.noreply.github.com>
2022-05-30 14:59:50 +0300

    Add files via upload

---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using Core;
+using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto : IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public int DailyPrice { get; set; }
+        public int CarFindexPoint { get; set; }
+        public int CustomerFindexPoint { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+}
